e1361c09807867166b82618c2837ffbdf11660b7
Антон <spamwarlook@gmail.com>
2017-06-16 16:00:09 +0300

    Добавил тэгированый кеш

--- src/Cache.php
+++ src/Cache.php
@@ -7,81 +7,103 @@ use Bitrix\Main\Data\StaticHtmlCache;
 use Closure;
 use CPHPCache;
 
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * (c) https://github.com/arrilot/bitrix-cacher
+ */
 class Cache
 {
-    /**
-     * Store closure's result in the cache for a given number of minutes.
-     *
-     * @param string  $key
-     * @param double  $minutes
-     * @param Closure $callback
-     * @param bool|string $initDir
-     *
-     * @return mixed
-     */
-    public static function remember($key, $minutes, Closure $callback, $initDir = false, $basedir = "cache")
-    {
-        $minutes = (double) $minutes;
-        if ($minutes <= 0) {
-            return $callback();
-        }
+    
+    const INIT_DIR = '/citrus/smartform';
 
-        $obCache = new CPHPCache();
-        if ($obCache->InitCache($minutes*60, $key, $initDir, $basedir)) {
-            $vars = $obCache->GetVars();
+	/**
+	 * Store closure's result in the cache for a given number of minutes.
+	 *
+	 * @param string  $key
+	 * @param double  $minutes
+	 * @param Closure $callback
+	 * @param bool|string $initDir
+	 *
+	 * @return mixed
+	 */
+	public static function remember($key, $minutes, Closure $callback, $initDir = null)
+	{
+		global $CACHE_MANAGER;
 
-            return $vars['cache'];
-        }
+		$initDir = self::INIT_DIR . '/' . $initDir . '/';
 
-        $obCache->StartDataCache();
-        try {
-            $cache = $callback();
-            $obCache->EndDataCache(['cache' => $cache]);
-        } catch (AbortCacheException $e) {
-            $obCache->AbortDataCache();
-            $cache = null;
-        }
+		$minutes = (double) $minutes;
+		if ($minutes <= 0) {
+			return $callback();
+		}
 
-        return $cache;
-    }
+		$obCache = new CPHPCache();
+		if ($obCache->InitCache($minutes*60, md5(serialize($key)), $initDir)) {
+			$vars = $obCache->GetVars();
 
-    /**
-     * Store closure's result in the cache for a long time.
-     *
-     * @param string $key
-     * @param Closure $callback
-     * @param bool|string $initDir
-     *
-     * @return mixed
-     */
-    public static function rememberForever($key, Closure $callback, $initDir = false, $basedir = "cache")
-    {
-        return static::remember($key, 99999999, $callback, $initDir, $basedir);
-    }
+			return $vars['cache'];
+		}
 
-    /**
-     * Flush cache for a specified dir.
-     *
-     * @param string $initDir
-     *
-     * @return bool
-     */
-    public static function flush($initDir = "")
-    {
-        return BXClearCache(true, $initDir);
-    }
+		$obCache->StartDataCache();
+		$CACHE_MANAGER->StartTagCache($initDir);
+		$cache = $callback();
+		$CACHE_MANAGER->EndTagCache();
+		$obCache->EndDataCache(array('cache' => $cache));
 
-    /**
-     * Flushes all bitrix cache.
-     *
-     * @return void
-     */
-    public static function flushAll()
-    {
-        $GLOBALS["CACHE_MANAGER"]->cleanAll();
-        $GLOBALS["stackCacheManager"]->cleanAll();
-        $staticHtmlCache = StaticHtmlCache::getInstance();
-        $staticHtmlCache->deleteAll();
-        BXClearCache(true);
-    }
+		return $cache;
+	}
+
+	/**
+	 * Store closure's result in the cache for a long time.
+	 *
+	 * @param string $key
+	 * @param Closure $callback
+	 * @param bool|string $initDir
+	 *
+	 * @return mixed
+	 */
+	public static function rememberForever($key, Closure $callback, $initDir = self::INIT_DIR)
+	{
+		return static::remember($key, 99999999, $callback, $initDir);
+	}
+
+	/**
+	 * Flush cache for a specified dir.
+	 *
+	 * @param string $initDir
+	 *
+	 * @return bool
+	 */
+	public static function flush($initDir = self::INIT_DIR)
+	{
+		return BXClearCache(true, $initDir);
+	}
+
+	/**
+	 * Flushes all bitrix cache.
+	 *
+	 * @return void
+	 */
+	public static function flushAll()
+	{
+		$GLOBALS["CACHE_MANAGER"]->cleanAll();
+		$GLOBALS["stackCacheManager"]->cleanAll();
+		$staticHtmlCache = StaticHtmlCache::getInstance();
+		$staticHtmlCache->deleteAll();
+		BXClearCache(true);
+	}
+
+	/**
+	 * Set tag cache for iblock-dependent result to clear tag cache
+	 *
+	 * @param int $iblockId
+	 */
+	public static function registerIblockCacheTag($iblockId)
+	{
+		if (defined("BX_COMP_MANAGED_CACHE") && \CModule::IncludeModule('iblock'))
+		{
+			\CIBlock::registerWithTagCache($iblockId);
+		}
+	}
 }
